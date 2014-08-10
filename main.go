00382fec88293e935c8dd5a016e8c840ff82d2f4
Alexey Baranov <a.baranov@office.ngs.ru>
2014-08-10 23:24:21 +0700

    Meal name added and output changed

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ type (
 	}
 	Meal struct {
 		Weight float64
+		Name   string
 		NutritionFacts
 	}
 )
@@ -25,7 +26,15 @@ func (f *NutritionFacts) String() string {
 		f.Carbohydrate, f.Calories)
 }
 
+func (m *Meal) String() string {
+	return fmt.Sprintf("%s/%2.2f/%s", m.Name, m.Weight, &m.NutritionFacts)
+}
+
 func main() {
+	var mealNameInput string
+	flag.StringVar(&mealNameInput, "n", "meal",
+		"Name of product you eaten")
+
 	var mealWeightInput string
 	flag.StringVar(&mealWeightInput, "w", "0",
 		"weight of eaten meal in grams")
@@ -35,7 +44,7 @@ func main() {
 		"protein/fat/carbohydrate/calories")
 
 	var dailyNormInput string
-	flag.StringVar(&dailyNormInput, "n", "90/90/340/2400",
+	flag.StringVar(&dailyNormInput, "d", "90/90/340/2400",
 		"protein/fat/carbohydrate/calories")
 
 	flag.Parse()
@@ -46,13 +55,15 @@ func main() {
 
 	meal := Meal{}
 	meal.Weight, _ = strconv.ParseFloat(mealWeightInput, 1)
+	meal.Name = mealNameInput
 	parseFacts(&meal.NutritionFacts, parsedFacts)
 
 	var dailyNorm = NutritionFacts{}
 	parseFacts(&dailyNorm, parsedDailyNorm)
 
 	calculated := calculatePercentage(meal, dailyNorm)
-	fmt.Print(&calculated)
+	meal.NutritionFacts = calculated
+	fmt.Print(&meal)
 }
 
 func calculatePercentage(meal Meal, dailyNorm NutritionFacts) NutritionFacts {
@@ -73,4 +84,5 @@ func parseFacts(facts *NutritionFacts, from []string) {
 	facts.Fat, _ = strconv.ParseFloat(from[2], 1)
 	facts.Carbohydrate, _ = strconv.ParseFloat(from[3], 1)
 	facts.Calories, _ = strconv.ParseFloat(from[4], 1)
+
 }
