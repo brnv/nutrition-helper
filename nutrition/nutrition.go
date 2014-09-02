7931f553712b8bca3600e206a9721f3053d69a15
Alexey Baranov <a.baranov@office.ngs.ru>
2014-09-02 11:44:37 +0700

    Update nutrition.go

--- nutrition/nutrition.go
+++ nutrition/nutrition.go
@@ -29,11 +29,11 @@ func Eat(form url.Values) {
 
 	f, _ := os.OpenFile("diary", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
 
-	facts.Protein, _ = strconv.ParseFloat(form.Get("proteinInput"), 2)
-	facts.Fat, _ = strconv.ParseFloat(form.Get("fatsInput"), 2)
-	facts.Carbohydrate, _ = strconv.ParseFloat(form.Get("carbohydratesInput"), 2)
-	facts.Calories, _ = strconv.ParseFloat(form.Get("caloriesInput"), 2)
-	facts.Weight, _ = strconv.ParseFloat(form.Get("weightInput"), 2)
+	facts.Protein, _ = strconv.ParseFloat(form.Get("proteinInput"), 32)
+	facts.Fat, _ = strconv.ParseFloat(form.Get("fatsInput"), 32)
+	facts.Carbohydrate, _ = strconv.ParseFloat(form.Get("carbohydratesInput"), 32)
+	facts.Calories, _ = strconv.ParseFloat(form.Get("caloriesInput"), 32)
+	facts.Weight, _ = strconv.ParseFloat(form.Get("weightInput"), 32)
 
 	f.Write([]byte(strconv.FormatInt(t, 10) + "\t" + form.Get("typeInput") +
 		"\t" + form.Get("productInput") + "\t" + form.Get("weightInput") +
